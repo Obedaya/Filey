21ec819b4bc7460778f23725884d7ab936808d83
Obedaya <yeneuro.mail@gmail.com>
2024-01-18 18:29:34 +0100

    Moved hasher functions into private

--- include/hasher.h
+++ include/hasher.h
@@ -16,12 +16,12 @@ public:
     explicit Hasher(Logger& logger);
     const unsigned char* getHashFile(const char *path);
     static constexpr std::size_t HASH_SIZE = 32;
-    const unsigned char* hashFilePath(const char *path);
-    const unsigned char* hashFileContent(const char *path);
-    const unsigned char* hashTwoHashes(const unsigned char* hash1, const unsigned char* hash2);
     static std::string binaryToHex(const unsigned char* data, size_t length);
 private:
     Logger& logger;
+    const unsigned char* hashFilePath(const char *path);
+    const unsigned char* hashFileContent(const char *path);
+    const unsigned char* hashTwoHashes(const unsigned char* hash1, const unsigned char* hash2);
 };
 
 
